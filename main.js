ece57cccd64d5ce30224a2ad7257eca6b2fad788
POOJACODER-CREATOR <71705069+POOJACODER-CREATOR@users.noreply.github.com>
2021-02-16 00:33:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+function one(){
+    document.getElementById("heerr").play();
+}
+function one1(){
+   var audio= document.getElementById("heerr");
+   audio.src="";
+    document.getElementById("heerr1").play();
+}
+function one2(){
+    var audio1= document.getElementById("heerr1");
+    audio1.src="";
+    document.getElementById("heerr2").play();
+}
+function one3(){
+    var audio2= document.getElementById("heerr2");
+    audio2.src="";
+    document.getElementById("heerr3").play();
+}
+function one4(){
+    var audio3= document.getElementById("heerr3");
+    audio3.src="";
+    document.getElementById("heerr4").play();
+}
+function one5(){
+    var audio4= document.getElementById("heerr4");
+    audio4.src="";
+    document.getElementById("heerr5").play();
+}
+function one6(){
+    var audio5= document.getElementById("heerr5");
+    audio5.src="";
+    document.getElementById("heerr6").play();
+}
+function one7(){
+    var audio6= document.getElementById("heerr6");
+    audio6.src="";
+    document.getElementById("heerr7").play();
+}
+function one8(){
+    var audio7= document.getElementById("heerr7");
+    audio7.src="";
+    document.getElementById("heerr8").play();
+}
+function one9(){
+    var audio8= document.getElementById("heerr8");
+    audio8.src="";
+    document.getElementById("heerr9").play();
+}
